a280b07f392fa7e46bc606416c3620dca27e18b3
zeleven <chengsnem@163.com>
2017-12-12 17:27:20 +0800

    Entity class implement

--- src/main/java/io/github/zeleven/lemon/Article.java
+++ src/main/java/io/github/zeleven/lemon/Article.java
@@ -1,4 +1,72 @@
 package io.github.zeleven.lemon;
 
+import javax.persistence.*;
+import java.util.Date;
+import java.util.Set;
+
+@Entity
+@Table(name = "articles")
 public class Article {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Integer id;
+    private String title;
+    @Column(columnDefinition = "TEXT")
+    private String content;
+    @Column(columnDefinition = "TEXT")
+    private String contentHtml;
+    private Date createTime;
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinTable(name = "article_tag",
+            joinColumns = @JoinColumn(name = "article_id"),
+            inverseJoinColumns = @JoinColumn(name = "tag_id"))
+    private Set<Tag> tags;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getContentHtml() {
+        return contentHtml;
+    }
+
+    public void setContentHtml(String contentHtml) {
+        this.contentHtml = contentHtml;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Set<Tag> getTags() {
+        return tags;
+    }
+
+    public void setTags(Set<Tag> tags) {
+        this.tags = tags;
+    }
 }
--- src/main/java/io/github/zeleven/lemon/Tag.java
+++ src/main/java/io/github/zeleven/lemon/Tag.java
@@ -1,4 +1,39 @@
 package io.github.zeleven.lemon;
 
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "tags")
 public class Tag {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Integer id;
+    private String name;
+    @ManyToMany(mappedBy = "tags")
+    private Set<Article> article;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Article> getArticle() {
+        return article;
+    }
+
+    public void setArticle(Set<Article> article) {
+        this.article = article;
+    }
 }
