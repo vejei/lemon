439e879d22c9e3ba1f983e892a47cf66e02bc959
zeleven <chengsnem@163.com>
2017-12-13 21:54:54 +0800

    Add method

--- src/main/java/io/github/zeleven/lemon/repositories/RoleRepository.java
+++ src/main/java/io/github/zeleven/lemon/repositories/RoleRepository.java
@@ -1,4 +1,10 @@
 package io.github.zeleven.lemon.repositories;
 
-public class RoleRepository {
+import io.github.zeleven.lemon.entities.Role;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository("roleRepository")
+public interface RoleRepository extends CrudRepository<Role, Long> {
+    Role findByName(String name);
 }
