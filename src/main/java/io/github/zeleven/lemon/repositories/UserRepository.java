7a57578ff932473950b4301be234f251d0ac2ee6
zeleven <chengsnem@163.com>
2017-12-13 21:55:21 +0800

    Add method

--- src/main/java/io/github/zeleven/lemon/repositories/UserRepository.java
+++ src/main/java/io/github/zeleven/lemon/repositories/UserRepository.java
@@ -1,4 +1,11 @@
 package io.github.zeleven.lemon.repositories;
 
-public interface UserRepository {
+import io.github.zeleven.lemon.entities.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository("userRepository")
+public interface UserRepository extends CrudRepository<User, Long> {
+    User findByEmail(String email);
+    User findByConfirmationToken(String findByConfirmationToken);
 }
