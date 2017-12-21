866a16bc6eb27d1e9fb2dcc3f13c4a1ef8ccc100
zeleven <chengsnem@163.com>
2017-12-21 21:58:34 +0800

    update

--- src/main/java/io/github/zeleven/lemon/repositories/ArticleRepository.java
+++ src/main/java/io/github/zeleven/lemon/repositories/ArticleRepository.java
@@ -1,7 +1,12 @@
 package io.github.zeleven.lemon.repositories;
 
 import io.github.zeleven.lemon.entities.Article;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-public interface ArticleRepository extends CrudRepository<Article, Long> {
+@Repository("articleRepository")
+public interface ArticleRepository extends CrudRepository<Article, Integer> {
+    Page<Article> findAll(Pageable pageable);
 }
