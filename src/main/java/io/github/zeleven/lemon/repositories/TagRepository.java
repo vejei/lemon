e781f9ac54fe7b86f627d9165027abc15d6c4f4e
zeleven <chengsnem@163.com>
2017-12-23 10:28:00 +0800

    Add repository

--- src/main/java/io/github/zeleven/lemon/repositories/TagRepository.java
+++ src/main/java/io/github/zeleven/lemon/repositories/TagRepository.java
@@ -1,4 +1,9 @@
 package io.github.zeleven.lemon.repositories;
 
-public class TagRepository {
+import io.github.zeleven.lemon.entities.Tag;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository("tagRepository")
+public interface TagRepository extends CrudRepository<Tag, Integer> {
 }
