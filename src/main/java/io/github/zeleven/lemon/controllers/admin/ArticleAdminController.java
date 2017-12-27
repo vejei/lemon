f2fa75b3d0240f3dc6885d53587c2e6f8de697fc
zeleven <chengsnem@163.com>
2017-12-27 11:37:09 +0800

    complete article controller

--- src/main/java/io/github/zeleven/lemon/controllers/admin/ArticleAdminController.java
+++ src/main/java/io/github/zeleven/lemon/controllers/admin/ArticleAdminController.java
@@ -3,25 +3,47 @@ package io.github.zeleven.lemon.controllers.admin;
 import io.github.zeleven.lemon.entities.Article;
 import io.github.zeleven.lemon.services.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping("admin/article")
-public class ArticleController {
+public class ArticleAdminController {
     @Autowired
     private ArticleService articleService;
 
-    @RequestMapping(value = "/write", method = RequestMethod.GET)
-    public String writePage(Model model) {
-        model.addAttribute("article", new Article());
-        return "admin/article/write";
-    }
-
     @RequestMapping(value = "/write", method = RequestMethod.POST)
     public String create() {
-        return "redirect:/admin";
+        Article article = new Article();
+        article.setTitle("无标题");
+        articleService.saveArticle(article);
+        return "redirect:/admin/article";
+    }
+
+    @RequestMapping(value = "/delete", method = RequestMethod.POST)
+    public String delete(@RequestParam(name = "aid_input") Integer aid) {
+        articleService.deleteArticle(aid);
+        return "redirect:/admin/article";
+    }
+
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseEntity<Object> updateArticle(@RequestBody Article a) {
+        Map<String, String> result = new HashMap<>();
+        Article article = articleService.findOne(a.getId());
+        if (article != null) {
+            article.setTitle(a.getTitle());
+            article.setContent(a.getContent());
+            articleService.saveArticle(article);
+            result.put("msg", "文章已更新");
+        } else {
+            result.put("msg", "文章不存在");
+        }
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 }
