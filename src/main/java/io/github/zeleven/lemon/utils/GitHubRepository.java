567e24659184aa1bc2e6470a915c6016801a3dd5
zeleven <chengsnem@163.com>
2017-12-29 20:11:32 +0800

    gson util class

---
+++ src/main/java/io/github/zeleven/lemon/utils/GitHubRepository.java
@@ -0,0 +1,87 @@
+package io.github.zeleven.lemon.utils;
+
+import java.util.Date;
+
+public class GitHubRepository {
+    private int id;
+    private String name;
+    private String full_name;
+    private String html_url;
+    private String description;
+    private Date updated_at;
+    private int stargazers_count;
+    private String language;
+    private int forks_count;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFull_name() {
+        return full_name;
+    }
+
+    public void setFull_name(String full_name) {
+        this.full_name = full_name;
+    }
+
+    public String getHtml_url() {
+        return html_url;
+    }
+
+    public void setHtml_url(String html_url) {
+        this.html_url = html_url;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Date updated_at) {
+        this.updated_at = updated_at;
+    }
+
+    public int getStargazers_count() {
+        return stargazers_count;
+    }
+
+    public void setStargazers_count(int stargazers_count) {
+        this.stargazers_count = stargazers_count;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public int getForks_count() {
+        return forks_count;
+    }
+
+    public void setForks_count(int forks_count) {
+        this.forks_count = forks_count;
+    }
+}
