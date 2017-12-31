d3052d24817b2afc12642e2077a131f00507e647
zeleven <chengsnem@163.com>
2017-12-31 14:20:55 +0800

    completed controller

--- src/main/java/io/github/zeleven/lemon/controllers/admin/TadAdminController.java
+++ src/main/java/io/github/zeleven/lemon/controllers/admin/TadAdminController.java
@@ -1,4 +1,39 @@
 package io.github.zeleven.lemon.controllers.admin;
 
+import io.github.zeleven.lemon.entities.Tag;
+import io.github.zeleven.lemon.services.TagService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("admin/tag")
 public class TadAdminController {
+    @Autowired
+    private TagService tagService;
+
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    public String create(@RequestParam(name = "name") String name) {
+        Tag tag = new Tag();
+        tag.setName(name);
+        tagService.saveTag(tag);
+        return "redirect:/admin/tag";
+    }
+
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    public String update(@RequestParam(name = "id") Integer id,
+                         @RequestParam(name = "name") String name) {
+        Tag tag = tagService.findOne(id);
+        tag.setName(name);
+        tagService.saveTag(tag);
+        return "redirect:/admin/tag";
+    }
+
+    @RequestMapping(value = "/delete", method = RequestMethod.POST)
+    public String delete(@RequestParam(name = "tid") Integer id) {
+        tagService.deleteTag(id);
+        return "redirect:/admin/tag";
+    }
 }
