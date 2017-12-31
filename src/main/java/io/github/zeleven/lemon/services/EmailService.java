99472fe13f58dcd4fe0e06880168fef0fca963c7
zeleven <chengsnem@163.com>
2017-12-31 14:30:39 +0800

    update

--- src/main/java/io/github/zeleven/lemon/services/EmailService.java
+++ src/main/java/io/github/zeleven/lemon/services/EmailService.java
@@ -1,4 +1,18 @@
 package io.github.zeleven.lemon.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Service("emailService")
 public class EmailService {
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Async
+    public void sendEmail(SimpleMailMessage email) {
+        mailSender.send(email);
+    }
 }
