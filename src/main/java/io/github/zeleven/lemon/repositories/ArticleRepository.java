4a5eb6ea51cb2b62c5a777576b2b0860bb26eb4f
zeleven <chengsnem@163.com>
2017-12-31 14:12:29 +0800

    change super class

--- src/main/java/io/github/zeleven/lemon/repositories/ArticleRepository.java
+++ src/main/java/io/github/zeleven/lemon/repositories/ArticleRepository.java
@@ -3,10 +3,10 @@ package io.github.zeleven.lemon.repositories;
 import io.github.zeleven.lemon.entities.Article;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository("articleRepository")
-public interface ArticleRepository extends CrudRepository<Article, Integer> {
+public interface ArticleRepository extends JpaRepository<Article, Integer> {
     Page<Article> findAll(Pageable pageable);
 }
