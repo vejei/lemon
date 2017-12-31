4238a16812918143f3eb0a7a97bcac9f91ce6f08
zeleven <chengsnem@163.com>
2017-12-31 14:17:10 +0800

    completed controller

--- src/main/java/io/github/zeleven/lemon/controllers/ArticleController.java
+++ src/main/java/io/github/zeleven/lemon/controllers/ArticleController.java
@@ -1,7 +1,22 @@
 package io.github.zeleven.lemon.controllers;
 
+import io.github.zeleven.lemon.entities.Article;
+import io.github.zeleven.lemon.services.ArticleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ArticleController {
+    @Autowired
+    private ArticleService articleService;
+
+    @GetMapping(value = "/article/{articleId}")
+    public String articleDetail(Model model, @PathVariable Integer articleId) {
+        Article article = articleService.findOne(articleId);
+        model.addAttribute("article", article);
+        return "detail";
+    }
 }
