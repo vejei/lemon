a36c88efd76f0734492aaa0b54a14a3bb7d83504
zeleven <chengsnem@163.com>
2017-12-31 14:31:36 +0800

    completed entity class

--- src/main/java/io/github/zeleven/lemon/entities/Role.java
+++ src/main/java/io/github/zeleven/lemon/entities/Role.java
@@ -1,4 +1,40 @@
 package io.github.zeleven.lemon.entities;
 
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "role_id")
+    private Long id;
+    private String name;
+    @OneToMany(mappedBy = "role", cascade = CascadeType.ALL)
+    private Set<User> users;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
 }
