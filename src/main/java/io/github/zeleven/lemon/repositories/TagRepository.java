7e9e9ab4f9a0f57715d8bbe2001754e2c4c26051
zeleven <chengsnem@163.com>
2017-12-31 14:13:23 +0800

    change super class

--- src/main/java/io/github/zeleven/lemon/repositories/TagRepository.java
+++ src/main/java/io/github/zeleven/lemon/repositories/TagRepository.java
@@ -1,9 +1,9 @@
 package io.github.zeleven.lemon.repositories;
 
 import io.github.zeleven.lemon.entities.Tag;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository("tagRepository")
-public interface TagRepository extends CrudRepository<Tag, Integer> {
+public interface TagRepository extends JpaRepository<Tag, Integer> {
 }
