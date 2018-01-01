604b5d31a3121b2ceafec126c045535c9cbdcc96
zeleven <chengsnem@163.com>
2018-01-01 15:07:06 +0800

    insert admin before running

--- src/main/java/io/github/zeleven/lemon/DataLoader.java
+++ src/main/java/io/github/zeleven/lemon/DataLoader.java
@@ -1,6 +1,8 @@
 package io.github.zeleven.lemon;
 
+import io.github.zeleven.lemon.entities.Role;
 import io.github.zeleven.lemon.entities.User;
+import io.github.zeleven.lemon.repositories.RoleRepository;
 import io.github.zeleven.lemon.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
@@ -13,10 +15,18 @@ public class DataLoader implements ApplicationRunner {
     @Autowired
     private UserService userService;
     @Autowired
+    private RoleRepository roleRepository;
+    @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        Role roleExists = roleRepository.findByName("ADMIN");
+        if (roleExists == null) {
+            Role role = new Role();
+            role.setName("ADMIN");
+            roleRepository.save(role);
+        }
         User userExists = userService.findByEmail("admin@test.com");
         if (userExists == null) {
             User user = new User();
@@ -24,6 +34,9 @@ public class DataLoader implements ApplicationRunner {
             user.setEmail("admin@test.com");
             user.setPassword(bCryptPasswordEncoder.encode("123456"));
             user.setActive(1);
+
+            Role role = roleRepository.findByName("ADMIN");
+            user.setRole(role);
             userService.saveUser(user);
         }
     }
